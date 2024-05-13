7adc088c79e8846ea231f85bca86711e07899c86
EC2 Default User <ec2-user@ip-172-31-75-232.ec2.internal>
2024-05-13 18:20:46 +0000

    nuevos cambios

--- angular-prueba/src/app/app.module.ts
+++ angular-prueba/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
-import { LocalSorageDataComponent } from './local-sorage-data/local-sorage-data.component';
 import { DeleteItemComponent } from './delete-item/delete-item.component';
 import { LocalStorageDataComponent } from './local-storage-data/local-storage-data.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTaskComponent,
-    LocalSorageDataComponent,
     DeleteItemComponent,
-    LocalStorageDataComponent
+    LocalStorageDataComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule,

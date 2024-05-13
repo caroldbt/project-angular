b7a2784adf7123e30fa6eb8c2169711a0e5b3f59
EC2 Default User <ec2-user@ip-172-31-75-232.ec2.internal>
2024-05-13 18:14:05 +0000

    nuevo cambio

--- angular-prueba/src/app/app-routing.module.ts
+++ angular-prueba/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddTaskComponent } from './add-task/add-task.component';
-import { LocalStorageDataComponent } from './local-sorage-data/local-sorage-data.component';
+import { LocalStorageDataComponent } from './local-storage-data/local-storage-data.component';
 
 const routes: Routes = [
   {
@@ -9,7 +9,7 @@ const routes: Routes = [
   },
   
   {
-    path:'my-tasks', component:LocalStorageDataComponent
+    path:'my-tasks', component: LocalStorageDataComponent
   },
   {
     path:'**', component:AddTaskComponent,pathMatch:'full'
--- angular-prueba/src/app/app.module.ts
+++ angular-prueba/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { LocalSorageDataComponent } from './local-sorage-data/local-sorage-data.component';
 import { DeleteItemComponent } from './delete-item/delete-item.component';
+import { LocalStorageDataComponent } from './local-storage-data/local-storage-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTaskComponent,
     LocalSorageDataComponent,
-    DeleteItemComponent
+    DeleteItemComponent,
+    LocalStorageDataComponent
   ],
   imports: [
     BrowserModule,
